72f6719ec4b97f21b5186df4959678c0433721c8
jaalkab <jaalkab@ithaca.jaalkab.mx>
2017-02-06 20:38:13 -0600

    Assignment 1 Basic scaffold DONE :: v0.0.3

--- index.php
+++ index.php
@@ -4,15 +4,22 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap 101 Template</title>
+        <title>Assignment 1: System Setup 1 | Home</title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
-    <body>
-        <h1>Hello, world!</h1>
+    <body id="home">
+        <section class="container">
+            <div class="content row">
+                <section class="main col col-lg-8">
 
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+                </section> <!-- main -->
+                <section class="sidebar col col-lg-4">
+
+                </section> <!-- sidebar -->
+            </div> <!-- content -->
+        </section> <!-- container -->
+
         <script src="js/jquery.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
