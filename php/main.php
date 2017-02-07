ec444c327955154d3157e1d4fc48e6d1843a67cd
jaalkab <jaalkab@ithaca.jaalkab.mx>
2017-02-07 14:45:11 -0600

    Assignment 1 New text updated :: v0.0.8

--- php/main.php
+++ php/main.php
@@ -2,7 +2,10 @@
     <div class="col-lg-12">
         <h1>About me</h1>
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in pulvinar libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in pulvinar libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            Shalhavit Simcha Cohen is a motivational speaker and educator. She was the project manager of Dr Tal Ben-Shahar and the Harvard Happiness Class, and is still constantly developing educational projects in the field of Positive Psychology. Her recent lectures were at Tufts and CUNY. She is a very passionate educator certified by the Israeli government. Always looking for creative ways to introduce knowledge of well being. In addition Shalhavit-Simcha is currently a television producer&host, developing film with American Public TV pitching to MTV, HBO and more. A dynamic personality, experienced leader and team player.
+        </p>
+        <p>
+            A fun fact about her: Shalhavit was the 2011 Miss Fitness of Israel, and has much knowledge in the world of physical health and nutrition. She believes psychology is not only neck and up, and not everything is in our heads but our bodies as well.
         </p>
         <h1>Random Quote</h1>
     </div>
