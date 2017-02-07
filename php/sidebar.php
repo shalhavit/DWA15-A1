5dc3dd7d183252e8032948ef69b7e9ca3326b8b2
jaalkab <jaalkab@ithaca.jaalkab.mx>
2017-02-07 15:01:34 -0600

    Assignment 1 Warnings correction :: v0.0.9

--- php/sidebar.php
+++ php/sidebar.php
@@ -1,6 +1,7 @@
 <div class="content row">
     <div class="col-lg-12">
         <section id="imgsidebar">
+            <h3 class="text-center">Hi I am Shalhavit</h3>
             <img src="../img/artwork/20170128_112136.jpg" class="img-responsive" alt="Shalhavit picture">
         </section> <!-- imgsidebar -->
     </div>
