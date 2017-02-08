0a6eae11c0881f8e2e05f5c57ffe9f4d8a596d10
Shalhavit-Simcha Cohen <shalhavit@ithaca.jaalkab.mx>
2017-02-08 11:59:27 -0600

    Assignment 1 Image setup :: v0.0.25

--- php/sidebar.php
+++ php/sidebar.php
@@ -1,5 +1,5 @@
 <div class="content row">
     <div id="imgsidebar" class="col-lg-12">
-        <img src="../img/artwork/20170128_112136.jpg" class="img-responsive" alt="Shalhavit picture">
+          <img src="../img/artwork/20170128_112136.jpg" class="img-responsive img-rounded" alt="Shalhavit picture">
     </div>
 </div>
