bc6615f0cb1f9a91dba0e0073391cd881662c467
jaalkab <jaalkab@ithaca.jaalkab.mx>
2017-02-08 10:08:06 -0600

    Assignment 1 Random quote update :: v0.0.16

--- php/qgenerator.php
+++ php/qgenerator.php
@@ -1,7 +1,7 @@
 <?php
-$file= "php/quotes.txt";
-$quotes = file($file);
+$inputfile= "php/quotes.txt";
+$myquotes = file($inputfile);
 srand((double)microtime()*1000000);
-$randomquote = rand(0, count($quotes)-1);
-echo $quotes[$randomquote];
+$randomquote = rand(0, count($myquotes)-1);
+echo $myquotes[$randomquote];
 ?>
