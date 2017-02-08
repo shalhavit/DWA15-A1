4920e74fa1e45d24778a2aaada0a9e70487b60b8
Shalhavit-Simcha Cohen <shalhavit@ithaca.jaalkab.mx>
2017-02-08 10:47:05 -0600

    Assignment 1 Random quote update :: v0.0.18

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
             <?php include "php/header.php";?>
         </div> <!-- header -->
         <section class="container">
-            <h2>About me</h2>
+            <h2>About me Part 2</h2>
             <div class="content row">
                 <section class="main col col-lg-8">
                     <div class="row">
